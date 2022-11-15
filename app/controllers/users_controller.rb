0ad8d4699112f685725437a1271ecfd22c1a40d7
Paul Yang <paulyang00@gmail.com>
2022-11-15 16:51:04 -0500

    small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,20 @@ class UsersController < ApplicationController
         render json: User.all, status: :ok
     end
 
+    def show
+        render json: find_user, status: :ok
+    end
+
     def create
         render json: User.create!(user_params), status: :created
     end
 
+
     private
+
+    def find_user
+        User.find(params[:id])
+    end
 
     def user_params
         params.permit(:name, :password)
