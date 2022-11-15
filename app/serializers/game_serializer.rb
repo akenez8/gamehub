729c22533a3c211d43c2929458ff74b197211f8d
Paul Yang <paulyang00@gmail.com>
2022-11-15 15:48:47 -0500

    seeds planted

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :title, :platform, :rented
+  attributes :id, :title, :platform, :rented, :image
 end
--- db/migrate/20221114200351_create_rentals.rb
+++ db/migrate/20221114200351_create_rentals.rb
@@ -1,8 +1,6 @@
 class CreateRentals < ActiveRecord::Migration[6.1]
   def change
     create_table :rentals do |t|
-      t.integer :user_id
-      t.integer :game_id
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :game, null: false, foreign_key: true
 
