757243e9a412c87ad4b3fefb2c956b8206169d28
peter <+>
2022-11-17 12:43:33 -0500

    conditional navbar renders

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -11,16 +11,14 @@ function Navbar({ user, setUser, setLogout }) {
     return(
     <header className="navHeader">
       <div className="logoutButtonDiv">
-        <button className="logoutButton" onClick={handleLogout}>Logout</button>
+        { user ? <button className="logoutButton" onClick={handleLogout}>Logout</button> : null} 
       </div>
     <div className="NavDiv">
         <NavLink className="nav" style={{ marginRight: "10px" }} exact to="/" >Home</NavLink>
         {user ? null : <NavLink className="nav" style={{ marginRight: "10px" }} to="/Login" >Login</NavLink> }
-         
-        {/* <NavLink className="nav" style={{ marginRight: "10px" }} to="/Login" >Login</NavLink> */}
         <NavLink className="nav" style={{ marginRight: "10px" }} to="/RentAGame" >Rent-A-Game</NavLink>
         <NavLink className="nav" style={{ marginRight: "10px" }} to="/MyRentals" >My Rentals</NavLink>
-        <NavLink className="nav" style={{ marginRight: "10px" }} to="/SignUp" >SignUp</NavLink>
+        {user ? null : <NavLink className="nav" style={{ marginRight: "10px" }} to="/SignUp" >SignUp</NavLink>}
     </div>
     </header>
     )
