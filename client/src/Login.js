3c49dc547b68a358e72a4194ae89bbe43c0e9b7e
peter <+>
2022-11-16 22:59:22 -0500

    more css changes!

--- client/src/Login.js
+++ client/src/Login.js
@@ -19,19 +19,25 @@ function Login({ setLogin }) {
   
     return (
       <form className="loginForm" onSubmit={handleSubmit}>
+        <p>
         <input 
           className="usernameInput"
           type="text"
           value={name}
           placeholder="username"
           onChange={(e) => setName(e.target.value)}/>
+          </p>
+          <p>
           <input 
           className="passwordInput"
           type="text"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}/>
+          </p>
         <button className="loginButton"type="submit">Login</button>
+        <p>Dont have an account?</p>
+        <button className="createAccountButton" type="submit">Create Account</button>
       </form>
       
     );
