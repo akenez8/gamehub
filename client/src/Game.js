c1efa3234bc04b9376bb4aa24348a2f4606e8473
Chloe Scheffler <chloescheffler@gmail.com>
2022-11-18 00:34:01 -0500

    css

--- client/src/Game.js
+++ client/src/Game.js
@@ -4,11 +4,18 @@ function Game({title, image, platform}){
     return(
         <div className="gameCard">
             <span className="bold">{title}</span>
-            <img className="cardImage" id="game_img" src={image} width="220" height="180" alt="{title}"/>
-            <p className="console">{platform}</p>
-            <p>
-            <button>Rent</button>
+            <div class="flip-box">
+                <div class="flip-box-inner">
+                    <div class="flip-box-front">
+                            <img className="cardImage" id="game_img" src={image} width="220" height="180" alt="{title}"/>
+                    </div>
+                </div>
+            </div> 
+            <p className="console">{platform}
             </p>
+            <p>
+                <button className="rentButton">Rent</button>
+            </p>   
         </div>
     )
 }
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Navbar({ user, setUser, setLogout }) {
-    function handleLogout() {
+  let history = useHistory();
+    // function handleLogout() {
+    //     fetch("/logout", {
+    //       method: "DELETE",
+    //     })
+    //     .then(res => {
+    //       if (res.ok) {
+    //         console.log('delete')
+    //       }
+    //     })
+    //   }
+    
+      
+      function handleLogout() {
         fetch("/logout", {
           method: "DELETE",
-        })
-        .then(res => {
-          if (res.ok) {
-            console.log('delete')
-          }
-        })
-      }
-    
+        });
+          history.push(`/`);
+          setUser([]);
+}
+
     return(
     <header className="navHeader">
       <div className="logoutButtonDiv">
-        { user ? <button className="logoutButton" onClick={handleLogout}>Logout</button> : null} 
+        <button className="logoutButton" onClick={handleLogout}>Logout</button>
       </div>
-    <div className="NavDiv">
-        <NavLink className="nav" style={{ marginRight: "10px" }} exact to="/" >Home</NavLink>
-        <NavLink className="nav" style={{ marginRight: "10px" }} to="/RentAGame" >Rent-A-Game</NavLink>
-        <NavLink className="nav" style={{ marginRight: "10px" }} to="/MyRentals" >My Rentals</NavLink>
-        {user ? null : <NavLink className="nav" style={{ marginRight: "10px" }} to="/Login" >Login</NavLink> }
-        {user ? null : <NavLink className="nav" style={{ marginRight: "10px" }} to="/SignUp" >SignUp</NavLink>}
-    </div>
+      <div>
+        <NavLink  className="nav" class="active" style={{ marginRight: "10px" }} exact to="/" >Home</NavLink>
+        <NavLink  className="nav" style={{ marginRight: "10px" }} to="/RentAGame" >Rent-A-Game</NavLink>
+        <NavLink  className="nav" style={{ marginRight: "10px" }} to="/MyRentals" >My Rentals</NavLink>
+        {user ? null : <NavLink  style={{ marginRight: "10px" }} to="/Login" >Login</NavLink> }
+        {user ? null : <NavLink  style={{ marginRight: "10px" }} to="/SignUp" >SignUp</NavLink>}
+      </div>
     </header>
     )
 }
