89daefb11c542df678d9da26ac65e28029e59056
Gian Carlo De Castro <Giancld92@gmail.com>
2022-11-17 19:33:52 -0600

    Problems

--- client/src/MyRentals.js
+++ client/src/MyRentals.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Game from "./Game";
 import UserRentalGames from "./UserRentalGames";
 
 function MyRentals({rentals, setRentals}){
 
-    const userRentedGames = rentals.filter((rental) => {
+    const userRentedGames = rentals.map((rental) => {
         return(
             (rental.game)
-        )
-    })
+        )})
 
-    const userGameCard = userRentedGames.map((game) => {
+    const filteredUserRentedGames = userRentedGames.filter((user) => {
+        return (Rental.user_id === user.id)})
+
+    const userGameCard = filteredUserRentedGames.map((game) => {
         return(
             <UserRentalGames title={game.title} image={game.image} platform={game.platform}/>
-        )
-    }
-    )
+        )})
+
     return(
         <div>
             <p className="MyRentalsTitle">Your Rented Games</p>
